92b8f36f03dd50aaad35ed725bb973497f01d163
lzz <972321170@qq.com>
2025-03-25 21:08:46 +0800

    实现本地服务注册器

--- provider/src/main/java/com/sysu/provider/EasyProviderExample.java
+++ provider/src/main/java/com/sysu/provider/EasyProviderExample.java
@@ -1,7 +1,10 @@
 package com.sysu.provider;
 
+import com.sysu.register.LocalRegister;
 import com.sysu.server.HttpServer;
 import com.sysu.server.impl.VertxHttpServer;
+import com.sysu.service.UserService;
+import com.sysu.service.impl.UserServiceImpl;
 
 /**
  * 简易服务提供者示例
@@ -9,6 +12,9 @@ import com.sysu.server.impl.VertxHttpServer;
 public class EasyProviderExample {
 
     public static void main(String[] args) {
+        // 本地注册
+        LocalRegister.register(UserService.class.getName(), UserServiceImpl.class);
+
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
