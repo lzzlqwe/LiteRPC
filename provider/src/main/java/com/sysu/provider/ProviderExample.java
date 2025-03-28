09a586243baefdae52b21d4e2989576e03d26140
lzz <972321170@qq.com>
2025-03-28 15:08:27 +0800

    完成注册中心基本实现（服务消费者远程调用服务提供者）

--- provider/src/main/java/com/sysu/provider/ProviderExample.java
+++ provider/src/main/java/com/sysu/provider/ProviderExample.java
@@ -1,7 +1,12 @@
 package com.sysu.provider;
 
 import com.sysu.RpcApplication;
+import com.sysu.config.RegistryConfig;
+import com.sysu.config.RpcConfig;
+import com.sysu.model.ServiceMetaInfo;
 import com.sysu.register.LocalRegister;
+import com.sysu.register.Registry;
+import com.sysu.register.RegistryFactory;
 import com.sysu.server.HttpServer;
 import com.sysu.server.impl.VertxHttpServer;
 import com.sysu.service.UserService;
@@ -13,7 +18,22 @@ public class ProviderExample {
         RpcApplication.init();
 
         // 本地注册
-        LocalRegister.register(UserService.class.getName(), UserServiceImpl.class);
+        String serviceName = UserService.class.getName(); //获取服务名
+        LocalRegister.register(serviceName, UserServiceImpl.class);
+
+        // 注册服务到服务中心
+        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry()); //获取注册中心
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName(serviceName);
+        serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
+        serviceMetaInfo.setServicePort(rpcConfig.getServerPort());
+        try {
+            registry.register(serviceMetaInfo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
